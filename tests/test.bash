aea5fabbbe09cfaf96865d0b324ae1cd0ac32c9b
Daniel Barella <dan.barella@gmail.com>
2015-03-24 14:45:28 -0400

    Modified tests to use the correct filename.

--- tests/test.bash
+++ tests/test.bash
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-for _ in `seq 10`; do python i_am_a.py; done
+for _ in `seq 10`; do python i_am.py; done
--- tests/test.fish
+++ tests/test.fish
@@ -1,3 +1,3 @@
 #!/usr/bin/env fish
 
-for _ in (seq 10); python i_am_a.py; end
+for _ in (seq 10); python i_am.py; end
